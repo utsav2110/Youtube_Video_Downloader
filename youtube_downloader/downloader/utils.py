751e9ae137e6c8068dd7cac8b4affad6bfb5c618
utsav2110 <solankiutsavdharmeshkumar@gmail.com>
2025-03-14 15:18:30 +0530

    download fix

--- youtube_downloader/downloader/utils.py
+++ youtube_downloader/downloader/utils.py
@@ -1,17 +1,25 @@
 import yt_dlp
 from django.core.cache import cache
-import os  
+import os
+from datetime import datetime
 
 CACHE_KEY = "download_progress"
 
-
 def download_video(url, audio_only=False):
+    current_date = datetime.now().strftime("%Y%m%d")
+    
     ydl_opts = {
         "outtmpl": os.path.join(
             os.path.expanduser("~"), "Downloads", "%(title)s.%(ext)s"
-        ), 
+        ),
         "progress_hooks": [progress_hook],
+        "postprocessor_args": [
+            '-metadata', f'date={current_date}',
+            '-metadata', f'creation_time={current_date}'
+        ],
+        "writethumbnail": True,
     }
+    
     if audio_only:
         ydl_opts["format"] = "bestaudio"
         ydl_opts["postprocessors"] = [
@@ -23,6 +31,10 @@ def download_video(url, audio_only=False):
         ]
     else:
         ydl_opts["format"] = "best"
+        ydl_opts["postprocessors"] = [{
+            'key': 'FFmpegMetadata',
+            'add_metadata': True,
+        }]
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
